4aa5c152b7584276878c79f37a7de5cacf5a9277
ifilatov <ivan@sysadmin76.ru>
2025-12-09 14:09:46 +0300

    Modernize build configuration
    - Update webpack.config.js to modern syntax
    - Update tsconfig.json for Preact JSX support
    - Remove deprecated tslint.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
 const path = require('path');
-const webpack = require('webpack');
-const TSLintPlugin = require('tslint-webpack-plugin');
 
-function getWebviewConfig(env) {
-	/** @type webpack.Configuration */
-	let webview = {
-		name: 'webiew',
-		mode: env.production ? 'production' : 'development',
+function getWebviewConfig(mode) {
+	/** @type {import('webpack').Configuration} */
+	return {
+		name: 'webview',
+		mode: mode,
 		entry: {
 			index: './src/views/issueWebView/index.tsx'
 		},
@@ -18,44 +16,30 @@ function getWebviewConfig(env) {
 					exclude: /node_modules/
 				},
 				{
-					test: /\.css/,
+					test: /\.css$/,
 					use: ['style-loader', 'css-loader']
-				},
-				{
-					test: /\.svg/,
-					use: ['svg-inline-loader']
 				}
 			]
 		},
 		resolve: {
-			extensions: ['.tsx', '.ts', '.js', '.svg']
+			extensions: ['.tsx', '.ts', '.js']
 		},
-		devtool: !env.production ? 'inline-source-map' : undefined,
+		devtool: mode === 'development' ? 'inline-source-map' : false,
 		output: {
 			filename: '[name].js',
 			path: path.resolve(__dirname, 'media')
-		},
-		plugins: [
-			new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-			new TSLintPlugin({
-				files: ['./src/**/*.ts']
-			})
-		]
+		}
 	};
-
-	return webview;
 }
 
 /**
- *
- * @param {*} env
- * @returns webpack.Configuration
+ * @param {string} mode
+ * @returns {import('webpack').Configuration}
  */
-function getExtensionConfig(env) {
-	/** @type webpack.Configuration */
-	let config = {
+function getExtensionConfig(mode) {
+	return {
 		name: 'extension',
-		mode: env.production ? 'production' : 'development',
+		mode: mode,
 		target: 'node',
 		entry: {
 			extension: './src/extension.ts'
@@ -66,52 +50,26 @@ function getExtensionConfig(env) {
 					test: /\.tsx?$/,
 					use: 'ts-loader',
 					exclude: /node_modules/
-				},
-				// FIXME: apollo-client uses .mjs, which imposes hard restrictions
-				// on imports available from other callers. They probably didn't know
-				// this. They just used .mjs because it seemed new and hip.
-				//
-				// We should either fix or remove that package, then remove this rule,
-				// which introduces nonstandard behavior for mjs files, which are
-				// terrible. This is all terrible. Everything is terrible.👇🏾
-				{
-					test: /\.mjs$/,
-					include: /node_modules/,
-					type: "javascript/auto",
-				},
-				{
-					test: /\.gql/,
-					loader: 'graphql-tag/loader',
-					exclude: /node_modules/
 				}
 			]
 		},
 		resolve: {
-			extensions: ['.tsx', '.ts', '.js'],
-			alias: {
-				"node-fetch": path.resolve(__dirname, 'node_modules/node-fetch/lib/index.js'),
-			}
+			extensions: ['.tsx', '.ts', '.js']
 		},
-		devtool: !env.production ? 'source-map' : undefined,
+		devtool: mode === 'development' ? 'source-map' : false,
 		output: {
 			filename: '[name].js',
 			path: path.resolve(__dirname, 'media'),
-			libraryTarget: "commonjs",
-			devtoolModuleFilenameTemplate: 'file:///[absolute-resource-path]'
+			libraryTarget: 'commonjs2',
+			devtoolModuleFilenameTemplate: '../[resource-path]'
 		},
 		externals: {
-			'vscode': 'commonjs vscode',
-			'utf-8-validate': 'utf-8-validate',
-			'bufferutil': 'bufferutil',
-			'encoding': 'encoding'
+			vscode: 'commonjs vscode'
 		}
 	};
-
-	return config;
 }
 
-module.exports =  function(env) {
-	env = env || {};
-	env.production = !!env.production;
-	return [getExtensionConfig(env), getWebviewConfig(env)];
+module.exports = (env, argv) => {
+	const mode = argv.mode || 'development';
+	return [getExtensionConfig(mode), getWebviewConfig(mode)];
 };
